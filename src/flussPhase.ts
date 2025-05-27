10296041ed7925518f42bfb6a293a00db6723057
k4yr2 <kyrzkaya@gmail.com>
2025-05-27 13:58:10 +0300

    Refactor FlussPhase constructor to handle null mainFunc and improve initialization logic

--- src/flussPhase.ts
+++ src/flussPhase.ts
@@ -4,18 +4,24 @@ class FlussPhase {
     #type : MainPhaseType;
     #preType : PrePhaseType;
     #postType : PostPhaseType;
-    #mainFunc : FlussPhaseFunc;
+    #mainFunc : FlussPhaseFunc | null = null;
     #preFunc : FlussPhaseFunc | null = null;
     #postFunc : FlussPhaseFunc | null = null;
 
-    constructor(type: MainPhaseType, mainFunc: FlussPhaseFunc, preFunc: FlussPhaseFunc | null, postFunc: FlussPhaseFunc | null) {
+    constructor(type: MainPhaseType, mainFunc: null, preFunc: null, postFunc:  null);
+
+    constructor(type: MainPhaseType, mainFunc: FlussPhaseFunc, preFunc: FlussPhaseFunc | null, postFunc: FlussPhaseFunc | null);
+
+    constructor(type: MainPhaseType, mainFunc: FlussPhaseFunc | null, preFunc: FlussPhaseFunc | null, postFunc: FlussPhaseFunc | null) {
         this.#type = type;
         this.#preType = getPrePhase(type);
         this.#postType = getPostPhase(type);
 
-        this.#mainFunc = mainFunc;
-        this.#preFunc = preFunc;
-        this.#postFunc = postFunc;
+        if(mainFunc) {
+            this.#mainFunc = mainFunc;
+            this.#preFunc = preFunc;
+            this.#postFunc = postFunc;
+        }
     }
 
     // ------------------------- // -  - // ------------------------- //
