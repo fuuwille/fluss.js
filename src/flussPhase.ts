93a144879a4b8af5c10108af4dac075545dc2b6f
k4yr2 <kyrzkaya@gmail.com>
2025-05-27 09:38:38 +0300

    Rename Start phase to Begin and update related phases for clarity

--- src/flussPhase.ts
+++ src/flussPhase.ts
@@ -1,9 +1,9 @@
 enum FlussPhase {
     None = 0,
     Idle = 1 << 0,
-    Start = 1 << 1,
-    StartBefore = 1 << 2 | Start,
-    StartAfter = 1 << 3 | Start
+    Begin = 1 << 1,
+    PreBegin = 1 << 2 | Begin,
+    PostBegin = 1 << 3 | Begin,
 }
 
 export default FlussPhase;
