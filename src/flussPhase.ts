c81c71730b8286a235a9729dda36a54ce7d09a56
k4yr2 <kyrzkaya@gmail.com>
2025-05-27 12:24:51 +0300

    Enhance FlussPhase class by adding preType and postType properties with corresponding getters

--- src/flussPhase.ts
+++ src/flussPhase.ts
@@ -1,15 +1,27 @@
-import { MainPhaseType } from "./flussPhaseType";
+import { getPostPhase, getPrePhase, MainPhaseType, PostPhaseType, PrePhaseType } from "./flussPhaseType";
 
 class FlussPhase {
     #type : MainPhaseType;
+    #preType : PrePhaseType;
+    #postType : PostPhaseType;
 
     constructor(type: MainPhaseType) {
         this.#type = type;
+        this.#preType = getPrePhase(type);
+        this.#postType = getPostPhase(type);
     }
     
 
     public get type(): MainPhaseType {
         return this.#type;
+    }
+
+    public get preType(): PrePhaseType {
+        return this.#preType;
+    }
+
+    public get postType(): PostPhaseType {
+        return this.#postType;
     }
 }
 
