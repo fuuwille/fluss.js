c6320a11a5457a4cf634e459a073d9001e776a62
k4yr2 <kyrzkaya@gmail.com>
2025-05-27 15:54:15 +0300

    Add FlussActionBundle type definition to enhance action structure

--- src/flussAction.ts
+++ src/flussAction.ts
@@ -1,3 +1,11 @@
 type FlussAction = () => void | Promise<void>;
 
 export default FlussAction;
+
+// ------------------------------ // -  - // ------------------------------ //
+
+export type FlussActionBundle = {
+    main: FlussAction;
+    pre?: FlussAction | null;
+    post?: FlussAction | null;
+}
