e58a8471995da79f7a1b0523cd13baf201309130
k4yr2 <kyrzkaya@gmail.com>
2025-05-27 09:49:11 +0300

    Add PrePhases constant for enhanced state management

--- src/flussPhase.ts
+++ src/flussPhase.ts
@@ -22,3 +22,5 @@ export default FlussPhase;
 // ------------------------------ // -  - // ------------------------------ //
 
 export const MainPhases = FlussPhase.Idle | FlussPhase.Begin | FlussPhase.Running | FlussPhase.End | FlussPhase.Completed;
+
+export const PrePhases = FlussPhase.PreIdle | FlussPhase.PreBegin | FlussPhase.PreRunning | FlussPhase.PreEnd | FlussPhase.PreCompleted;
