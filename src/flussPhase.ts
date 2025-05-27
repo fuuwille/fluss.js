9828d5a858c82cf4708a38b5a8fd28975418e083
k4yr2 <kyrzkaya@gmail.com>
2025-05-27 09:40:10 +0300

    Add Running phases to FlussPhase enum for enhanced state management

--- src/flussPhase.ts
+++ src/flussPhase.ts
@@ -4,6 +4,9 @@ enum FlussPhase {
     Begin = 1 << 1,
     PreBegin = 1 << 2 | Begin,
     PostBegin = 1 << 3 | Begin,
+    Running = 1 << 4,
+    PreRunning = 1 << 5 | Running,
+    PostRunning = 1 << 6 | Running
 }
 
 export default FlussPhase;
