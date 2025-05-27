eebe3538d97fbc00484693f16772cd1b79a9fd85
k4yr2 <kyrzkaya@gmail.com>
2025-05-27 09:43:01 +0300

    Reorder FlussPhase enum values for improved clarity and consistency

--- src/flussPhase.ts
+++ src/flussPhase.ts
@@ -1,18 +1,20 @@
 enum FlussPhase {
     None = 0,
     Idle = 1 << 0,
-    Begin = 1 << 1,
-    PreBegin = 1 << 2 | Begin,
-    PostBegin = 1 << 3 | Begin,
-    Running = 1 << 4,
-    PreRunning = 1 << 5 | Running,
-    PostRunning = 1 << 6 | Running,
-    End = 1 << 7,
-    PreEnd = 1 << 8 | End,
-    PostEnd = 1 << 9 | End,
-    Completed = 1 << 10,
-    PreCompleted = 1 << 11 | Completed,
-    PostCompleted = 1 << 12 | Completed
+    PreIdle = 1 << 1 | Idle,
+    PostIdle = 1 << 2 | Idle,
+    Begin = 1 << 3,
+    PreBegin = 1 << 4 | Begin,
+    PostBegin = 1 << 5 | Begin,
+    Running = 1 << 6,
+    PreRunning = 1 << 7 | Running,
+    PostRunning = 1 << 8 | Running,
+    End = 1 << 9,
+    PreEnd = 1 << 10 | End,
+    PostEnd = 1 << 11 | End,
+    Completed = 1 << 12,
+    PreCompleted = 1 << 13 | Completed,
+    PostCompleted = 1 << 14 | Completed
 }
 
 export default FlussPhase;
