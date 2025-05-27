1c28d26b08458c703667367c27be88c0e9d440a1
k4yr2 <kyrzkaya@gmail.com>
2025-05-27 09:34:36 +0300

    Refactor FlussPhase class to enum for improved type safety

--- src/flussPhase.ts
+++ src/flussPhase.ts
@@ -1,5 +1,9 @@
-class FlussPhase {
-
+enum FlussPhase {
+    None = 0,
+    Idle = 1 << 0,
+    Start = 1 << 1,
+    StartBefore = 1 << 2 | Start,
+    StartAfter = 1 << 3 | Start
 }
 
 export default FlussPhase;
