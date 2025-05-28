5ca4964d6746b1da5e1b9b4a35c10ebabfba0973
k4yr2 <kyrzkaya@gmail.com>
2025-05-28 16:16:35 +0300

    Refactor FlussStage constructor to require phaseBundle and handle its initialization

--- src/flussStage.ts
+++ src/flussStage.ts
@@ -1,9 +1,26 @@
+import { FlussPhaseBundle } from "./flussPhase";
+
 class FlussStage {  
     #name : string;
+    #phaseBundle : FlussPhaseBundle;
 
-    constructor(name : string) {
+    constructor(name : string, phaseBundle : FlussPhaseBundle) {
         this.#name = name;
+
+        if(!phaseBundle) {
+            if(this.bindPhase) {
+                phaseBundle = this.bindPhase();
+            } else {
+                throw new Error(`FlussStage: No phase bundle provided for stage ${name}.`);
+            }
+        }
+
+        this.#phaseBundle = phaseBundle;
     }
+
+    // ------------------------- // -  - // ------------------------- //
+
+    protected bindPhase?(): FlussPhaseBundle;
 }
 
 export default FlussStage;
