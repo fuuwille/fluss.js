1cde52607d3b24551687d6533f4e5e27e83d64e9
k4yr2 <kyrzkaya@gmail.com>
2025-05-28 16:38:04 +0300

    Implement Fluss class with constructor to initialize stage bundle

--- src/fluss.ts
+++ src/fluss.ts
@@ -1,5 +1,11 @@
-class Fluss {
+import { FlussStageBundle } from "./flussStage";
 
+class Fluss {
+    #stageBundle : FlussStageBundle;
+
+    constructor(stageBundle: FlussStageBundle) {
+        this.#stageBundle = stageBundle;
+    }
 }
 
 export default Fluss;
