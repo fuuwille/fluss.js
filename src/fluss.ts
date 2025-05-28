5c54cfc916454b49d26f4e4bf3f9d8050d5a93f8
k4yr2 <kyrzkaya@gmail.com>
2025-05-28 19:49:47 +0300

    Refactor Fluss constructor to handle optional data and improve error handling

--- src/fluss.ts
+++ src/fluss.ts
@@ -3,7 +3,16 @@ import { FlussStageDef } from "./flussStage";
 class Fluss {
     #data : FlussData;
 
-    constructor(data: FlussData) {
+    constructor(data?: FlussData) {     
+        if(!data) {
+            if(this.bind) {
+                data = this.bind();
+            }
+            else {
+                throw new Error("Fluss: No stage definitions provided.");
+            }
+        }
+
         this.#data = data;
     }
 
@@ -15,6 +24,21 @@ class Fluss {
         return new Fluss(data);
     }
 }
+
+export abstract class FlussBound extends Fluss {
+    constructor() {
+        super();
+    }
+
+    protected bind(): FlussData {
+        return this.data();
+    }
+
+    // ------------------------- // -  - // ------------------------- //
+
+    public abstract data() : FlussData;
+}
+
 
 export default Fluss;
 
