5e27cbbfacb0df2f4f641934667ce3c0bb63e013
k4yr2 <kyrzkaya@gmail.com>
2025-05-30 11:02:29 +0300

    Refactor Fluss class to remove data property and constructor for improved simplicity; add FlussFlow class with data handling for enhanced functionality

--- src/fluss.ts
+++ src/fluss.ts
@@ -2,13 +2,11 @@ import { FlussPhaseDef, FlussPhaseSource } from "./flussPhase";
 import { FlussStageDef, FlussStageSource } from "./flussStage";
 
 class Fluss {
-    #data : FlussData;
 
-    constructor(data: FlussData) {     
-        this.#data = data;
+    private constructor() {     
     }
 
-    // ------------------------------ // -  - // ------------------------------ //
+    // ------------------------- // -  - // ------------------------- //
 
     public static stage(src : FlussStageSource, priority? : number) : FlussStageDef {
         return { src, priority }
@@ -20,15 +18,3 @@ class Fluss {
 }
 
 export default Fluss;
-
-// ------------------------------ // -  - // ------------------------------ //
-
-export type FlussData = {
-    [key: string] : FlussStageDef;
-}
-
-// ------------------------------ // -  - // ------------------------------ //
-
-export const createFluss = (data: FlussData): Fluss => {
-    return new Fluss(data);
-}
---
+++ src/flussFlow.ts
@@ -0,0 +1,24 @@
+import { FlussPhaseDef, FlussPhaseSource } from "./flussPhase";
+import { FlussStageDef, FlussStageSource } from "./flussStage";
+
+class FlussFlow {
+    #data : FlussFlowData;
+
+    constructor(data: FlussFlowData) {     
+        this.#data = data;
+    }
+}
+
+export default FlussFlow;
+
+// ------------------------------ // -  - // ------------------------------ //
+
+export type FlussFlowData = {
+    [key: string] : FlussStageDef;
+}
+
+// ------------------------------ // -  - // ------------------------------ //
+
+export const createFlow = (data: FlussFlowData): FlussFlow => {
+    return new FlussFlow(data);
+}
