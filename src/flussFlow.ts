8341e9d96484e12196f958b89dde7bd6b5daed62
k4yr2 <kyrzkaya@gmail.com>
2025-05-30 11:14:52 +0300

    Refactor FlussFlow class to initialize stages within the constructor and improve stage sorting logic

--- src/flussFlow.ts
+++ src/flussFlow.ts
@@ -1,10 +1,31 @@
-import { FlussStageDef } from "./flussStage";
+import FlussStage, { createStage, FlussStageDef } from "./flussStage";
 
 class FlussFlow {
     #data : FlussFlowData;
+    #stages : readonly FlussStage[];
 
     constructor(data: FlussFlowData, modifier? : FlussFlowModifier) {     
         this.#data = data;
+
+        const stages = Object.entries(data).map(([name, def]) => {
+            return createStage(name, def);
+        });
+
+        if (modifier) {
+            modifier(stages);
+        }
+
+        const sortedStages = stages.sort((a, b) => {
+            return (a.priority ?? Infinity) - (b.priority ?? Infinity);
+        });
+
+        this.#stages = [...sortedStages];
+    }
+
+    // ------------------------- // -  - // ------------------------- //
+
+    public get stages() : readonly FlussStage[] {
+        return this.#stages;
     }
 }
 
