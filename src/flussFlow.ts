ac38f8d5794df795cdee6f70e7cf1f6be57b2b80
k4yr2 <kyrzkaya@gmail.com>
2025-05-30 18:20:49 +0300

    Add asynchronous methods for run and finalize in FlussFlow

--- src/flussFlow.ts
+++ src/flussFlow.ts
@@ -46,6 +46,15 @@ class FlussFlow {
         return true;
     }
 
+    public async startAsync() : Promise<boolean> {
+        if (!this.#current) {
+            return Promise.resolve(false);
+        }
+
+        await this.#current.startAsync();
+        return true;
+    }
+
     public run() : boolean {
         if (!this.#current) {
             return false;
@@ -55,12 +64,38 @@ class FlussFlow {
         return true;
     }
 
+    public async runAsync() : Promise<boolean> {
+        if (!this.#current) {
+            return Promise.resolve(false);
+        }
+
+        await this.#current.runAsync();
+        return true;
+    }
+
     public finalize() : boolean {
         if (!this.#current) {
             return false;
         }
 
         this.#current.finalize();
+        const currentIndex = this.#stages.indexOf(this.#current);
+
+        if (currentIndex >= 0 && currentIndex < this.#stages.length - 1) {
+            this.#current = this.#stages[currentIndex + 1];
+        } else {
+            this.#current = null; // No more stages to process
+        }
+
+        return true;
+    }
+
+    public async finalizeAsync() : Promise<boolean> {
+        if (!this.#current) {
+            return Promise.resolve(false);
+        }
+
+        await this.#current.finalizeAsync();
         const currentIndex = this.#stages.indexOf(this.#current);
 
         if (currentIndex >= 0 && currentIndex < this.#stages.length - 1) {
