3b4f8bb309ebdf76805e0e814b016e07a8f4367c
k4yr2 <kyrzkaya@gmail.com>
2025-05-31 13:36:54 +0300

    Refactor fluss class to update fixedFlow and flexFlow methods to return FlussFlowDef for improved type consistency

--- src/fluss.ts
+++ src/fluss.ts
@@ -1,4 +1,4 @@
-import FlussFlow, { FlussFlowData, FlussFlowFixedKey, FlussFlowFlexKey, FlussFlowState } from "./flussFlow";
+import FlussFlow, { FlussFlowData, FlussFlowDef, FlussFlowFixedKey, FlussFlowFlexKey, FlussFlowState } from "./flussFlow";
 import { FlussStageData, FlussStageDef } from "./flussStage";
 
 class fluss {
@@ -7,12 +7,12 @@ class fluss {
 
     // ------------------------- // -  - // ------------------------- //
 
-    public static fixedFlow = <TKey extends string, TState extends FlussFlowState = FlussFlowState>(state : TState, data: FlussFlowData<FlussFlowFixedKey<TKey>>) : FlussFlow<FlussFlowFixedKey<TKey>, TState> => {
-        return new FlussFlow<FlussFlowFixedKey<TKey>, TState>(state, data);
+    public static fixedFlow = <TKey extends string>(data: FlussFlowData<FlussFlowFixedKey<TKey>>) : FlussFlowDef<FlussFlowFixedKey<TKey>> => {
+        return { src : data };
     }
 
-    public static flexFlow = <TState extends FlussFlowState = FlussFlowState>(state : TState, data: FlussFlowData<FlussFlowFlexKey>) : FlussFlow<FlussFlowFlexKey, TState> => {
-        return new FlussFlow<FlussFlowFlexKey, TState>(state, data);
+    public static flexFlow = (data: FlussFlowData<FlussFlowFlexKey>) : FlussFlowDef<FlussFlowFlexKey> => {
+        return { src : data }
     }
 
     // ------------------------- // -  - // ------------------------- //
