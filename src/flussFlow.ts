662135e7c02de813125e3fd46001f1bcdc1b2148
k4yr2 <kyrzkaya@gmail.com>
2025-05-31 09:39:53 +0300

    Refactor FlussFlow to initialize stages in constructor and expose stages getter

--- src/flussFlow.ts
+++ src/flussFlow.ts
@@ -1,18 +1,32 @@
-import { FlussStageData } from "./flussStage";
+import FlussStage, { FlussStageData } from "./flussStage";
 
 class FlussFlow<TState extends FlussFlowState = FlussFlowState> {
     #state: TState;
     #data: FlussFlowData;
 
+    // ---------- // -  - // ---------- //
+
+    #stages : readonly FlussStage[];
+
     public constructor(state: TState, data: FlussFlowData) {
         this.#state = state;
         this.#data = data;
+
+        const stages = Object.entries(data).map(([name, def]) => {
+            return new FlussStage({ flow: this, name }, def);
+        });
+
+        this.#stages = Object.freeze(stages);
     }
 
     // ------------------------- // -  - // ------------------------- //
 
     public get state() : TState {
         return this.#state;
+    }
+
+    public get stages() : readonly FlussStage[] {
+        return this.#stages;
     }
 }
 
