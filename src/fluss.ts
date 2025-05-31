fcc7f39d4843b79188ef0437fbb0dd5869d058b4
k4yr2 <kyrzkaya@gmail.com>
2025-05-31 10:25:54 +0300

    Add flexFlow method to fluss class for flexible FlussFlow instance creation

--- src/fluss.ts
+++ src/fluss.ts
@@ -1,4 +1,4 @@
-import FlussFlow, { FlussFlowData, FlussFlowState } from "./flussFlow";
+import FlussFlow, { FlussFlowData, FlussFlowKey, FlussFlowState } from "./flussFlow";
 
 class fluss {
     private constructor() {
@@ -8,6 +8,10 @@ class fluss {
 
     public static fixedFlow = <TState extends FlussFlowState = FlussFlowState>(state : TState, data: FlussFlowData<string>) : FlussFlow<string, TState> => {
         return new FlussFlow<string, TState>(state, data);
+    }
+
+    public static flexFlow = <TKey extends FlussFlowKey, TState extends FlussFlowState = FlussFlowState>(state : TState, data: FlussFlowData<TKey>) : FlussFlow<TKey, TState> => {
+        return new FlussFlow<TKey, TState>(state, data);
     }
 }
 
