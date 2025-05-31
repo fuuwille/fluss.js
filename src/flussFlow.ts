55e7b44ce8aeac85386cbcd001ddcb1d377e851f
k4yr2 <kyrzkaya@gmail.com>
2025-05-31 09:10:43 +0300

    Add FlussFlowState type definition and format FlussFlowData

--- src/flussFlow.ts
+++ src/flussFlow.ts
@@ -13,3 +13,9 @@ export default FlussFlow;
 export type FlussFlowData = {
     [key: string]: FlussStageData;
 }
+
+// ------------------------------ // -  - // ------------------------------ //
+
+export type FlussFlowState = {
+    [key : string] : any;
+}
